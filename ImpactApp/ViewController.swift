25a196de6442f42bc7717ef6805a017b9ada45f1
Heyzues <jacobsalinas111@gamil.com>
2019-01-10 12:10:42 -0700

    Initial Commit

---
+++ ImpactApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImpactApp
+//
+//  Created by Salinas, Jesus on 1/10/19.
+//  Copyright © 2019 Salinas, Jesus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
